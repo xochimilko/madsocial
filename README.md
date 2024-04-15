30fb471f1b437ee571c2576f750454a19c630b18
xochimilko <kluvig@gmail.com>
2024-04-15 12:05:09 -0400

    added all files

---
+++ README.md
@@ -0,0 +1,3 @@
+# madsocialproj2
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/madsocialproj2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.madsocialproj2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
